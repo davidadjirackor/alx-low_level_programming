f110d8fd0aad5016e2c7b3c54bd5d9c2e08abff9
David Tetteh Adjirackor <davidadjirackor@gmail.com>
2022-06-04 03:20:39 +0000

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - prints out the alphabet
+* Description: alphabet is in lowercase except for 'qe'
+* Return: 0
+*/
 int main(void)
 {
-int y;
 
-for (y = 97; y <= 122; y++)
-{
-<<<<<<< HEAD
-       if (m == 101 || m == 113)
-=======
-if (y == 101 || y == 113)
->>>>>>> c38d008b18676f16774155151031a0fcc1c46752
-{
-}
-else
-{
-putchar(y);
-}
-}
-putchar('\n');
-return (0);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	putchar('\n');
+	return (0);
 }
